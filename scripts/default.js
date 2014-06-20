1422ec01ffb1f08724ba7cca6a4f82c11f54d1e9
Caleb Jares <cable729@gmail.com>
2014-06-19 22:22:32 -0500

    added skill section

--- scripts/default.js
+++ scripts/default.js
@@ -5,6 +5,7 @@
     animateChevron();
     setupFancyIcons();
     setupSmoothScroll();
+    setupKnobs();
     
     $('.block-header').fitText(1.15, { minFontSize: '56px' });
   });
@@ -12,6 +13,53 @@
   window.onresize = function(){
     $('#intro-screen').height(window.innerHeight);
   };
+  
+  function setupKnobs() {
+    $('.dial').knob({
+      readOnly: true,
+      width: '200',
+      displayInput: false,
+      thickness: '0.15',
+      draw : function () {
+          var a = this.angle(this.cv)  // Angle
+              , sa = this.startAngle          // Previous start angle
+              , sat = this.startAngle         // Start angle
+              , ea                            // Previous end angle
+              , eat = sat + a                 // End angle
+              , r = true;
+
+          this.g.lineWidth = this.lineWidth;
+
+          this.o.cursor
+              && (sat = eat - 0.3)
+              && (eat = eat + 0.3);
+
+          if (this.o.displayPrevious) {
+              ea = this.startAngle + this.angle(this.value);
+              this.o.cursor
+                  && (sa = ea - 0.3)
+                  && (ea = ea + 0.3);
+              this.g.beginPath();
+              this.g.strokeStyle = this.previousColor;
+              this.g.arc(this.xy, this.xy, this.radius - this.lineWidth, sa, ea, false);
+              this.g.stroke();
+          }
+
+          this.g.beginPath();
+          this.g.strokeStyle = r ? this.o.fgColor : this.fgColor ;
+          this.g.arc(this.xy, this.xy, this.radius - this.lineWidth, sat, eat, false);
+          this.g.stroke();
+
+          this.g.lineWidth = 2;
+          this.g.beginPath();
+          this.g.strokeStyle = this.o.fgColor;
+          this.g.arc(this.xy, this.xy, this.radius - this.lineWidth + 1 + this.lineWidth * 2 / 3, 0, 2 * Math.PI, false);
+          this.g.stroke();
+
+          return false;
+      }
+    });
+  }
   
   function setupFancyIcons(){
     $('.fancy-icon.square-to-circle').hover(function(e){
