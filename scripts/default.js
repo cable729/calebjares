6c297408efd6dc29921e94f628ea3cf390825eed
Caleb Jares <cable729@gmail.com>
2014-06-22 15:38:45 -0500

    added nav bar and portfolio. lots of other stuff

--- scripts/default.js
+++ scripts/default.js
@@ -1,15 +1,13 @@
-(function(){
-  'use strict';
-  
+(function(){  
   $(function(){
     animateChevron();
     setupFancyIcons();
     setupSmoothScroll();
     setupKnobs();
     setupKnobAnimations();
-    setupWorksSlideshow();
-    setupNavBar();
+    setupEvents();
     doResize();
+    carouselNormalization();
     
     $('.block-header').fitText(1.15, { minFontSize: '56px' });
   });
@@ -27,23 +25,40 @@
     }
   }
   
-  function setupNavBar() {
-    $('.navbar a').hover(
-      function over() {
-        $(this).stop().transition({ rotateX: '0.1turn' }, 200);
-      },
-      function out() {
-        $(this).stop().transition({ rotateX: '0' }, 200);        
-      }
-    );
-  }
+//  thanks to https://coderwall.com/p/uf2pka
+  function carouselNormalization() {
+    var items = $('#portfolio-carousel .item'), //grab all slides
+        heights = [], //create empty array to store height values
+        tallest; //create variable to make note of the tallest slide
+
+    if (items.length) {
+        function normalizeHeights() {
+            items.each(function() { //add heights to array
+                heights.push($(this).height()); 
+            });
+            tallest = Math.max.apply(null, heights); //cache largest value
+            items.each(function() {
+                $(this).css('min-height',tallest + 'px');
+            });
+        };
+        normalizeHeights();
+
+        $(window).on('resize orientationchange', function () {
+            tallest = 0, heights.length = 0; //reset vars
+            items.each(function() {
+                $(this).css('min-height','0'); //reset min-height
+            }); 
+            normalizeHeights(); //run it again 
+        });
+    }
+    }
   
-  function setupWorksSlideshow() {
-    $('.slideshow').slick({
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 5000
+  function setupEvents() {
+    $('.make-it-happen').click(function() {
+      $.smoothScroll({
+        scrollTarget: '#contact-me'
+      });
+      return false;
     });
   }
   
@@ -108,14 +123,14 @@
   
   function setupFancyIcons(){
     $('.fancy-icon.square-to-circle').hover(function(e){
-      $(this).transition({ rotate: '0.25turn' }, 300, 'ease');
-    }, function(e){
-      $(this).transition({ rotate: '0turn' }, 300, 'ease');
-    });
-    $('.fancy-icon.spin').hover(function(e){
       $(this).transition({ borderRadius: '30px', rotate: '-0.5turn' }, 400, 'ease');
     }, function(e){
       $(this).transition({ borderRadius: '4px', rotate: '0turn' }, 400, 'ease');
+    });
+    $('.fancy-icon.spin').hover(function(e){
+      $(this).transition({ rotate: '0.25turn' }, 300, 'ease');
+    }, function(e){
+      $(this).transition({ rotate: '0turn' }, 300, 'ease');
     });
     $('.fancy-icon.flip').hover(function(e){
       $(this).transition({ rotateX: '-0.5turn' }, 400, 'ease');
@@ -135,6 +150,6 @@
   };
 
   function setupSmoothScroll(){
-    $('a').smoothScroll();
+    $('a').smoothScroll({offset: -50});
   };
 })();
