ccfa09d76ca46f123ddd49b6ecb7ad0668d84af6
Caleb Jares <cable729@gmail.com>
2014-06-23 07:48:44 -0500

    contact info flares, so do technology spans

--- scripts/default.js
+++ scripts/default.js
@@ -1,11 +1,13 @@
-(function(){  
-  $(function(){
+(function() {  
+  $(function() {
     animateChevron();
     setupFancyIcons();
     setupSmoothScroll();
+    setupKnobClicks();
     setupKnobs();
     setupKnobAnimations();
-    setupEvents();
+    setupTechnologyClicks();
+    setupHireMe();
     doResize();
     carouselNormalization();
     
@@ -32,34 +34,64 @@
         tallest; //create variable to make note of the tallest slide
 
     if (items.length) {
-        function normalizeHeights() {
-            items.each(function() { //add heights to array
-                heights.push($(this).height()); 
-            });
-            tallest = Math.max.apply(null, heights); //cache largest value
-            items.each(function() {
-                $(this).css('min-height',tallest + 'px');
-            });
-        };
-        normalizeHeights();
+      function normalizeHeights() {
+          items.each(function() { //add heights to array
+              heights.push($(this).height()); 
+          });
+          tallest = Math.max.apply(null, heights); //cache largest value
+        items.each(function() {
+              $(this).css('min-height',tallest + 'px');
+          });
+      };
+      normalizeHeights();
 
-        $(window).on('resize orientationchange', function () {
-            tallest = 0, heights.length = 0; //reset vars
-            items.each(function() {
-                $(this).css('min-height','0'); //reset min-height
-            }); 
-            normalizeHeights(); //run it again 
-        });
+      $(window).on('resize orientationchange', function () {
+          tallest = 0, heights.length = 0; //reset vars
+          items.each(function() {
+              $(this).css('min-height','0'); //reset min-height
+          }); 
+          normalizeHeights(); //run it again 
+      });
     }
-    }
+  }
   
-  function setupEvents() {
+  function setupHireMe() {
     $('.make-it-happen').click(function() {
       $.smoothScroll({
         scrollTarget: '#contact-me'
       });
+      activateHireSpans(0);
       return false;
     });
+    $('#hire-me a').click(function() {
+      activateHireSpans(400);
+      return true;
+    });
+  }
+  
+  function activateHireSpans(delay) {
+    $('.hire').delay(delay)
+              .transition({ backgroundColor: '#ff452d' }, 1000)
+              .delay(500)
+              .transition({ backgroundColor: '#fff' }, 2000);
+  }
+  
+  function setupKnobClicks() {
+    $('.skill').click(function() {
+      var type = $(this).data('type');
+      $.smoothScroll({
+        scrollTarget: '#my-timeline',
+        offset: -50
+      });
+      $('.technologies li.' + type)
+              .transition({ backgroundColor: '#63b2ff' }, 1000)
+              .delay(2500)
+              .transition({ backgroundColor: '#fff' }, 2000);
+    });
+  }
+  
+  function setupTechnologyClicks() {
+    
   }
   
   function setupKnobAnimations() {
